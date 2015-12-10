97d857179ad737d2cf8767adc1d7a000432bef7f
alessandro.ferlin <alessandro.ferlin@thron.com>
2015-12-10 10:10:16 +0100

    add project xtest-core-model

---
+++ thron-core/xtest-core-model/src/main/scala/it/newvision/nvp/test/services/model/response/MTestBaseClass.scala
@@ -0,0 +1,22 @@
+package it.newvision.nvp.test.services.model.response
+import _root_.java.util._ 
+import _root_.java.lang.{Integer,Boolean,Long,Double,Float,Short}  
+//#SWG#import com.wordnik.swagger.annotations._ 
+import _root_.scala.beans.BeanProperty 
+import javax.xml.bind.annotation._ 
+
+/* ************************
+*  GENERATED CLASS
+*  DO NOT APPLY ANY CHANGES
+****************************/
+@XmlRootElement(name="MTestBaseClass") 
+@XmlType(name="MTestBaseClass")
+//#SWG#@ApiModel(description = """""")
+class MTestBaseClass extends Serializable {
+
+	//#SWG#@ApiModelProperty(value = """""" ,required = true)
+	@BeanProperty 
+	var id: String =_
+	def withid(p:String):MTestBaseClass ={ 	this.id = p; 	this }
+
+}
